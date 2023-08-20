3e7ef19537bf40f2afcb41953519a0e69b935ca2
Theo <theo.lettermann@gmail.com>
2023-08-20 19:39:28 +0200

    project setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,3 @@
-import { useState } from "react";
-
-function MyButton({ clickCount, onClick }) {
-  return (
-    <>
-      <div> You clicked this button {clickCount} times. </div>
-      <button onClick={onClick}>Click me</button>
-    </>
-  );
-}
-export default function App() {
-  const [clickCount, setClickCount] = useState(0);
-
-  function handleClick() {
-    console.log("Button clicked!");
-    setClickCount(clickCount + 1);
-  }
-
-  return (
-    <div>
-      <h1>Counters that update together</h1>
-      <MyButton clickCount={clickCount} onClick={handleClick} />
-      <MyButton clickCount={clickCount} onClick={handleClick} />
-    </div>
-  );
+export default function Square() {
+  return <button className="square">X</button>;
 }
