0658bf11c134a8f1bbaa8b198ed9fcb38d729e07
Theo <theo.lettermann@gmail.com>
2023-08-20 19:03:37 +0200

    print list of items with conditions in JSX style

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-function MyButton() {
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
+];
+
+
+export default function ShoppingList() {
+  const content = products.map(product =>
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? 'magenta' : 'darkgreen'
+      }}
+    >
+      {product.title}
+    </li>
+  )
   return (
-    <button>I'm a button </ button>
-  );
-}
-export default function MyApp() {
-  return (
-    <div>
-      <h1>Welcome to my app</h1>
-      <MyButton />
-    </div>
+    <ul>{content}</ul>
   )
 }
