81cc8a5d50f0831e41d8f27890f165661718ef3e
Theo <theo.lettermann@gmail.com>
2023-08-21 21:35:30 +0200

    add board and replace broken commits

--- src/App.js
+++ src/App.js
@@ -1,3 +1,40 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import { useState } from "react";
+
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+export default function Board() {
+  const [isXTurn, setIsXTurn] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleSquareClick(squareIndex) {
+    const tempSquares = [...squares]; // shallow copy
+
+    tempSquares[squareIndex] = isXTurn ? "X" : "O";
+    setSquares(tempSquares);
+    setIsXTurn(!isXTurn);
+  }
+  return (
+    <>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleSquareClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleSquareClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleSquareClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleSquareClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleSquareClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleSquareClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleSquareClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleSquareClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleSquareClick(8)} />
+      </div>
+    </>
+  );
 }
