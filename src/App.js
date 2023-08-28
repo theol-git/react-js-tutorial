d29b4f1c68cae7f2ea8a38df5534ab430e8f18bd
Theo <theo.lettermann@gmail.com>
2023-08-28 14:58:39 +0200

    functional tictactoe

--- src/App.js
+++ src/App.js
@@ -7,13 +7,34 @@ function Square({ value, onSquareClick }) {
     </button>
   );
 }
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 export default function Board() {
   const [isXTurn, setIsXTurn] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleSquareClick(squareIndex) {
-    if (squares[squareIndex] ){
-      return; 
+    if (squares[squareIndex] || calculateWinner(squares)) {
+      return;
     }
 
     const tempSquares = [...squares]; // shallow copy
@@ -22,8 +43,19 @@ export default function Board() {
     setSquares(tempSquares);
     setIsXTurn(!isXTurn);
   }
+
+  console.log("yo");
+  const winner = calculateWinner(squares);
+
+  let status;
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = `Next player: ${isXTurn ? "X" : "O"}`;
+  }
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleSquareClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleSquareClick(1)} />
